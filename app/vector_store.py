a7bb95e5a0c2982d860382d8e76dca7e63cdd132
mdsaibhossain <saibhossain5@gmail.com>
2025-08-04 16:29:01 +0600

    now update1

--- app/vector_store.py
+++ app/vector_store.py
@@ -6,3 +6,12 @@ model = SentenceTransformer('all-MiniLM-L6-v2')
 
 with open("/Users/mdsaibhossain/code/python/End-to-End RAG System/input.txt", "r") as f:
     text = f.read()
+
+chunks = [chunk.strip() for chunk in text.split("\n") if chunk.strip()]
+
+vectors = model.encode(chunks)
+
+dimension = vectors.shape[1]
+index = faiss.IndexFlatL2(dimension)
+index.add(np.array(vectors))
+print(f"Indexed {len(chunks)} text chunks.")
